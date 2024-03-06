90669fdfc5014e58c25598b665dc655b62672a28
Ramon <ramon.salaspi1.1@gmail.com>
2024-03-06 11:26:22 -0600

    Se crea segundo proyecto llamado weater app

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -3,7 +3,7 @@ import './styles/Error.css'
 const Error = () => {
   return (
     <article className="error">
-      <img className='error__image' src="/public/location-error.png" alt="" />
+      <img className='error__image' src="/location-error.png" alt="" />
       <h1 className='error__title'>No hay resultados del clima</h1>
       <p className='error__description'>Intente activando su ubicacón, para poder mostrar los datos del clima de su ciudad.</p>
     </article>
